d9f852aa4f292fd0135269b8907fcd929981869d
Pedro Ochsendorf Portugal <pedro.portugal@ufrgs.br>
2020-08-04 17:55:29 -0300

    Added normalized script and dataset version

---
+++ normalize_dataset.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+from sklearn.preprocessing import StandardScaler
+from PIL import Image 
+import numpy as np
+import pdb
+import os
+
+targets = ["test","train"]
+for tgt in targets:
+    img_dir = "./images/"+tgt+"/"
+    img_norm_dir = "./images_norm/"+tgt+"/"
+    for img_file in os.listdir(img_dir):
+        path = os.path.join(img_dir,img_file)
+        base_name, ext = img_file.split('.')
+        if path.endswith(".JPG") or path.endswith(".jpg"):
+            img = np.array(Image.open(path).convert('L'))
+            img_norm = (img-img.mean())/img.std()
+            norm_path = os.path.join(img_norm_dir,base_name)
+            np.save(norm_path,img_norm),
