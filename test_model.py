59198b7c10eedb44d0a22a6e29655a7e586bc204
Pedro Portugal <pedro.portugal@ufrgs.br>
2020-08-10 13:49:52 +0000

    Added bounded box to image display in test_model

--- test_model.py
+++ test_model.py
@@ -1,34 +1,99 @@
-import numpy as np
-from args import get_args
-from PIL import Image 
-import matplotlib.pyplot as plt
-import os
+def test_model():
 
-args = get_args()
+  import numpy as np
+  from args import get_args
+  from PIL import Image 
+  import matplotlib.pyplot as plt
+  
+  import matplotlib.patches as patches
+  import os
+  import pdb
 
-# Set keras verbosity
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'#'0' if args.verbose else '3'   
-import keras as kr
+  def get_image_paths(args):
+      np.random.seed(args.seed)
+      img_dir = args.data_dir+"images/test/"
+      file_list = os.listdir(img_dir)
+      img_file_list = [f for f in file_list if not(f.endswith('.xml'))]
+      index = np.random.randint(len(img_file_list))
+      img_path = os.path.join(img_dir,img_file_list[index])
 
-model = kr.models.load_model(args.model_name)
+      img_dir = args.data_dir+"images_norm/test/"
+      base_name, _ = img_file_list[index].split('.')
+      img_norm_path = os.path.join(img_dir,base_name+".npy")
 
-np.random.seed(args.seed)
-img_dir = args.data_dir+"images/test/"
-file_list = os.listdir(img_dir)
-img_file_list = [f for f in file_list if not(f.endswith('.xml'))]
-index = np.random.randint(len(img_file_list))
-img_path = os.path.join(img_dir,img_file_list[index])
+      return img_path, img_norm_path
 
-img_dir = args.data_dir+"images_norm/test/"
-base_name, _ = img_file_list[index].split('.')
-img_norm_path = os.path.join(img_dir,base_name+".npy")
-test=np.load(img_norm_path)
-test = test.reshape([1]+list(test.shape)+[1])
+  def post_process(args,prediction):
+      labels = ["ace","king","queen","jack","ten","nine"]
+      
+      if args.model_name == "yolo":
+        pred_labels,pred_pos = prediction
+        max_index = np.argmax(pred_labels)
+        pred_label = labels[max_index]
+        pred_conf  = pred_labels[0,max_index]
+        pred_pos = pred_pos*np.array([378,504,378,504])
+        pred_pos = [int(pos) for pos in pred_pos[0]]
+      if args.model_name == "classifier":
+        max_index  = np.argmax(prediction)
+        pred_label = labels[max_index]
+        pred_conf  = prediction[0,max_index] 
+        pred_pos = None
+      
+      return pred_label,pred_conf,pred_pos
 
-print("Test image : '",img_path,"'")
-plt.imshow(np.asarray(Image.open(img_path)))
-plt.show()
-print("Predicted: ",model.predict(test)) # shape required (1,504,378,1)
-if args.verbose:
-  model.summary()
-  print("Model input ",test)
+  def draw_bound_box(label,coord,box_color='m'):
+    x_ul, y_ul, x_lr, y_lr = coord
+    width_box, height_box  = x_lr-x_ul, y_lr-y_ul
+    width_label, height_label  = 60, -20
+    margin = 3
+    label_color = 'w'
+    label = "ace"
+
+    # Create a Rectangle patch
+    box = patches.Rectangle((x_ul,y_ul),width_box,height_box,linewidth=3,edgecolor=box_color,facecolor='none')
+    label_box = patches.Rectangle((x_ul,y_ul),width_label,height_label,linewidth=3,edgecolor=box_color,facecolor=box_color)
+
+    # Add the patch to the Axes
+    ax = plt.gcf().gca()
+    ax.add_patch(box)
+    ax.add_patch(label_box)
+    plt.text(x_ul, y_ul-margin,label,color=label_color)
+
+  def load_model(args):
+    # Set keras verbosity
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'#'0' if args.verbose else '3'   
+    import keras as kr
+    model = kr.models.load_model(args.model_name)
+
+    return model
+
+  def print_results(args,img_path,test,model):
+    prediction = model.predict(test) # shape required (1,504,378,1)
+    pred_label,pred_conf,pred_pos = post_process(args,prediction)
+    
+    print("Test image : '",img_path,"'")
+    plt.imshow(np.asarray(Image.open(img_path)))
+    if pred_pos is not None :
+      draw_bound_box(pred_label,pred_pos)
+    plt.show()
+  
+    print("Predicted Label '",pred_label,"' with '",pred_conf,"' confidence")
+    print("Predicted pos '",pred_pos,"'")
+
+    if args.verbose:
+      print("Predicted: ",prediction) 
+      model.summary()
+      print("Model input ",test)
+
+  args = get_args()
+
+  img_path, img_norm_path = get_image_paths(args)
+
+  test = np.load(img_norm_path)
+  test = test.reshape([1]+list(test.shape)+[1])
+
+  model = load_model(args)
+
+  print_results(args,img_path,test,model)
+
+test_model()
